6c695d7ca1bdce855621eb0e5d30f99e5a7f19ff
deton24 <34845212+deton24@users.noreply.github.com>
2025-03-01 21:00:24 +0100

    AttributeError: "'hop_length'" possbiel fix
    if model == 'SCNet':
            C = 485100
    ...
    yaml with hop_length: 441

--- inference.py
+++ inference.py
@@ -96,7 +96,10 @@ def demix_new(model, mix, device, config, dim_t=256):
     N = 2 # overlap 50%
     batch_size = 1
     mdx_window_size = dim_t
-    C = config.audio.hop_length * (mdx_window_size - 1)
+    if model == 'SCNet':
+        C = 485100
+    else:
+        C = config.audio.hop_length * (mdx_window_size - 1)
     fade_size = C // 100
     step = int(C // N)
     border = C - step
@@ -459,7 +462,7 @@ class EnsembleDemucsMDXMusicSeparationModel:
         elif model_name == "SCNetXL" and not hasattr(self, 'model_scnet'):
             print(f'Loading {model_name} into memory')   
             remote_url_ckpt = 'https://github.com/ZFTurbo/Music-Source-Separation-Training/releases/download/v1.0.13/model_scnet_ep_54_sdr_9.8051.ckpt'
-            remote_url_yaml = 'https://github.com/ZFTurbo/Music-Source-Separation-Training/releases/download/v1.0.13/config_musdb18_scnet_xl.yaml'
+            remote_url_yaml = 'https://github.com/deton24/Colab-for-new-MDX_UVR_models/releases/download/v1.0.0/config_musdb18_scnet_xl.yaml'
             self.model_scnet, self.config_scnet = self.load_model('model_scnet_ep_54_sdr_9.8051', remote_url_ckpt, remote_url_yaml, SCNet)
 
         elif model_name == "InstVoc" and not hasattr(self, 'model_mdxv3'):
